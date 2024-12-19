aa3d124f68813f493804f93357bd360b6afbd0bd
cksajil <sajilck@gmail.com>
2024-12-19 13:11:35 +0530

    updated code

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import joblib
 import streamlit as st
+from os import path
+import numpy as np
 
-st.title("Web app")
+st.title("Flower Classification App")
+
+filename = "lr_model.pkl"
+lr_model = joblib.load(path.join("model", filename))
+
+sl = st.number_input("Insert a sepel length")
+sw = st.number_input("Insert a sepel width")
+pl = st.number_input("Insert a petal length")
+pw = st.number_input("Insert a petal width")
+
+if st.button("Predict"):
+    pred = lr_model.predict(np.array([[sl, sw, pl, pw]]))
+    st.write("The flower is :", pred[0])
