588b41a578816fd868dc758b64a5d335b8d7515c
cksajil <sajilck@gmail.com>
2025-06-20 19:19:21 +0530

    cleaned main

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pickle
 import streamlit as st
-from os import path
 import numpy as np
+from os import path
 
 
 st.title("Flower Classification App")
 
+
 file_name = "lr_model.pkl"
-with open(path.join("model",file_name) , 'rb') as f:
+with open(path.join("model", file_name), "rb") as f:
     lr_model = pickle.load(f)
 
 sl = st.number_input("Insert a sepel length")
